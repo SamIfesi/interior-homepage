2340a65e24c5adbaadf930fef9ae20ca4e7e47a8
SamIfesi <Ekesamrose@gmail.com>
2026-02-22 19:02:12 +0100

    Add rating component with half-star functionality and new SVG icons

--- src/components/Navbar/Rating.jsx
+++ src/components/Navbar/Rating.jsx
@@ -1,12 +1,17 @@
-import rate from '/assets/icons/rating.svg';
+const rate = '/assets/icons/rating.svg';
+const rateEmpty = '/assets/icons/rating-empty.svg';
 
 export default function Rating() {
   return (
     <div className="rating">
       <div className="stars">
-        {[...Array(5)].map((_, index) => (
+        {[...Array(4)].map((_, index) => (
           <img key={index} src={rate} alt="star" />
         ))}
+        <span className="half-star-wrapper">
+          <img src={rateEmpty} alt="empty star" />
+          <img className="half-star" src={rate} alt="half star" />
+        </span>
       </div>
       <p>4.5/5 on 22,543 reviews</p>
     </div>
