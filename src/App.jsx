7913fa83069faf0a9e20def75c945970523d0589
SamIfesi <Ekesamrose@gmail.com>
2026-02-25 07:35:13 +0100

    Passes data prop to hero section component
    Enables the hero section to receive external data, improving
    flexibility and supporting dynamic content rendering in the UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
+import { heroData } from './components/Data/Data';
 
 function App() {
   return (
     <>
       <Navbar />
-      <HeroSection />
+      <HeroSection data={heroData} />
     </>
   );
 }
