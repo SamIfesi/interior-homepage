13128c691162961501da068ba55eca3eb26a2616
SamIfesi <Ekesamrose@gmail.com>
2026-02-25 06:59:13 +0100

    Refactors top bar to use centralized data source
    Improves maintainability and consistency by sourcing top bar content from a shared data module. Enables easier updates and potential localization for text and links.

--- src/components/Navbar/TopBar.jsx
+++ src/components/Navbar/TopBar.jsx
@@ -1,19 +1,18 @@
+import { topBarData } from '../Data/Data';
 import '../styles/Navtop.css';
-
 
 export default function TopBar() {
   return (
     <div className="navbar-top">
       <p>
-        Can’t see the item you're looking for?{' '}
-        <a href="#">Why not ask to have it made!</a>
+        {topBarData.text}
+        <a href="#">{topBarData.linkText}</a>
       </p>
 
       <ul className="top-links">
-        <li>Help</li>
-        <li>GB (£)</li>
-        <li>Find a store</li>
-        <li>Sign in or register</li>
+        {topBarData.links.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
       </ul>
     </div>
   );
