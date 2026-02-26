f3c06bcd14c9b77209fd8b86c04b4dac03607220
SamIfesi <Ekesamrose@gmail.com>
2026-02-26 21:52:58 +0100

    temp rename

---
+++ src/components/Footer/FooterTemp.jsx
@@ -0,0 +1,40 @@
+// import '../styles/Footer.css';
+import FooterColumn from './FooterColumn';
+import NewsLetter from './NewsLetter';
+import SocialIcons from './SocialIcons';
+import footerColumns from './footerData';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-left">
+          <h4>Connect with Us</h4>
+          <SocialIcons />
+          <NewsLetter />
+        </div>
+
+        <div className="footer-links">
+          {footerColumns.map((item, index) => (
+            <FooterColumn key={index} title={item.title} links={item.links} />
+          ))}
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>All rights reserved © MNMLST 2026</p>
+
+        <ul className="legal-links">
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Terms & Conditions</a>
+          </li>
+          <li>
+            <a href="#">Cookie Policy</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
