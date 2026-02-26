1ed88a9162e733f152a746e433e53b273dc5f2ac
SamIfesi <Ekesamrose@gmail.com>
2026-02-26 09:19:16 +0100

    Feat - Add footerData for array of informations for the footer

---
+++ src/components/Footer/footerData.js
@@ -0,0 +1,59 @@
+const footerColumns = [
+  {
+    title: 'Living Room',
+    links: [
+      { label: 'TV Stands', href: '#' },
+      { label: 'Sofas & Chairs', href: '#' },
+      { label: 'Coffee Tables', href: '#' },
+      { label: 'Side Tables', href: '#' },
+      { label: 'Accessories', href: '#' },
+      { label: 'Mirrors', href: '#' },
+    ],
+  },
+  {
+    title: 'Kitchen',
+    links: [
+      { label: 'TV Stands', href: '#' },
+      { label: 'Sofas & Chairs', href: '#' },
+      { label: 'Coffee Tables', href: '#' },
+      { label: 'Side Tables', href: '#' },
+      { label: 'Accessories', href: '#' },
+      { label: 'Mirrors', href: '#' },
+    ],
+  },
+  {
+    title: 'Bathroom',
+    links: [
+      { label: 'TV Stands', href: '#' },
+      { label: 'Sofas & Chairs', href: '#' },
+      { label: 'Coffee Tables', href: '#' },
+      { label: 'Side Tables', href: '#' },
+      { label: 'Accessories', href: '#' },
+      { label: 'Mirrors', href: '#' },
+    ],
+  },
+  {
+    title: 'Bedroom',
+    links: [
+      { label: 'TV Stands', href: '#' },
+      { label: 'Sofas & Chairs', href: '#' },
+      { label: 'Coffee Tables', href: '#' },
+      { label: 'Side Tables', href: '#' },
+      { label: 'Accessories', href: '#' },
+      { label: 'Mirrors', href: '#' },
+    ],
+  },
+  {
+    title: 'Garden',
+    links: [
+      { label: 'TV Stands', href: '#' },
+      { label: 'Sofas & Chairs', href: '#' },
+      { label: 'Coffee Tables', href: '#' },
+      { label: 'Side Tables', href: '#' },
+      { label: 'Accessories', href: '#' },
+      { label: 'Mirrors', href: '#' },
+    ],
+  },
+];
+
+export default footerColumns;
