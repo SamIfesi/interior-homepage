b3587b6283d48c135dba0da2c792a41f7a1530c9
SamIfesi <Ekesamrose@gmail.com>
2026-03-02 15:34:51 +0100

    Refactors newsletter component and styles for improved user experience

--- src/components/Footer/NewsLetter.jsx
+++ src/components/Footer/NewsLetter.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
+import message from '/assets/icons/message.svg';
+
 export default function NewsLetter() {
+  const [hasText, setHasText] = useState(false);
+
   return (
     <form className="newsletter" onSubmit={(e) => e.preventDefault()}>
-      <label htmlFor="newsletter-email">Subscribe to our newsletter</label>
-      <div className="newsletter-input-group">
+      <label htmlFor="newsletter-email">Stay in touch</label>
+      <p>
+        Join our email list to get all the latest news and offers right to your
+        inbox.
+      </p>
+      <div className={`newsletter-input-group${hasText ? ' has-text' : ''}`}>
         <input
           id="newsletter-email"
           type="email"
           placeholder="Enter your email"
+          onChange={(e) => setHasText(e.target.value.length > 0)}
         />
-        <button type="submit">Subscribe</button>
+        <button type="submit">
+          <img src={message} alt="message icon" />
+        </button>
       </div>
     </form>
   );
