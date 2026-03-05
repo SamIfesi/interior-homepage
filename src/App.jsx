6be303d2cc59ce1c98838b698bb2f057684e610b
SamIfesi <Ekesamrose@gmail.com>
2026-03-05 16:09:32 +0100

    Adds new section and improves data-driven rendering
    Integrates a handmade section to enhance content diversity and passes relevant data to customer reviews for more dynamic rendering.
    
    Improves maintainability and prepares for future content expansion.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
 import Footer from './components/Footer/FooterSection';
 import CustomerReviews from './components/CustomerReviews/CustomerReviews';
-import { heroData } from './components/Data/Data';
+import HandMade from './components/HandMade/HandMade';
+import { heroData, review, handMadeSec } from './components/Data/Data';
 
 function App() {
   return (
     <>
       <Navbar />
+      <HandMade data={handMadeSec} />
       <HeroSection data={heroData} />
-      <CustomerReviews />
+      <CustomerReviews data={review[0]} />
       <Footer />
     </>
   );
