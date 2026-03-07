58f380f001f4693e3257ee27c80a528e984596e7
SamIfesi <Ekesamrose@gmail.com>
2026-03-07 08:35:11 +0100

    feat: enhance CardSection with title and container for products

--- src/components/Cards/CardSection.jsx
+++ src/components/Cards/CardSection.jsx
@@ -1,11 +1,17 @@
 import Card from '../ui/CardCarousel';
+import CarouselNav from '../ui/CarouselNav';
 
 export default function CardSection({ products }) {
   return (
     <section className="card-section">
+      <h2 className="card-title">Explore our latest collection</h2>
+      <div className="card-container">
         {products.map((product, index) => (
           <Card key={index} product={product} />
         ))}
+      </div>
+      {/* <CarouselNav onPrev={handlePrev} onNext={handleNext} /> */}
+      <CarouselNav />
     </section>
   );
 }
